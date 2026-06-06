a3a03175d714aca6d467a917223d7429f5c95e44
RCN <teprachana11112007@gmail.com>
2026-06-06 13:52:25 +0700

    create models and migration

--- app/Models/orders.php
+++ app/Models/orders.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class orders extends Model
 {
-    
+    protected $fillable=[
+        'id',
+        'customer_id',
+        'table_id',
+        'waiter_id',
+        'cashier_id',
+        'cooker_id',
+        'discount_id',
+        'discount_applied',
+        'order_type',
+        'status',
+        'total_amount',
+        'discount_amount',
+        'final_amount',
+        'notes',
+        'created_at',
+        'updated_at',
+    ];
 }
