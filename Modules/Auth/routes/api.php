374e52391959b494660c608d71e938b151030341
thida rek <thidaarek@gmail.com>
2026-06-20 11:28:52 +0700

    crud-api-register

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -6,3 +6,10 @@ use Modules\Auth\Http\Controllers\AuthController;
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::apiResource('auths', AuthController::class)->names('auth');
 });
+Route::prefix('auth')->group(function () {
+    Route::get('/', [AuthController::class, 'index']);
+    Route::post('/register', [AuthController::class, 'store']);
+    Route::get('/{id}', [AuthController::class, 'show']);
+    Route::put('/{id}', [AuthController::class, 'update']);
+    Route::delete('/{id}', [AuthController::class, 'destroy']);
+});
